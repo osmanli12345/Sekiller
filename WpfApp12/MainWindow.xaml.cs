74146f01f080fa5e492fc48c7251b833a1cab1c4
Admin <Admin@DESKTOP-SC7U2PB>
2023-07-16 00:11:48 +0400

    Add project files.

---
+++ WpfApp12/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp12
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            Image image = (Image)button.Content;
+            
+
+            ImageViewer viewer = new ImageViewer(image.Source);
+            viewer.Show();
+        }
+        
+    }
+    public partial class ImageViewer : Window
+    {
+        public ImageViewer(ImageSource imageSource)
+        {
+            //InitializeComponent();
+            Image image = new Image();
+            image.Source = imageSource;
+            image.Stretch=System.Windows.Media.Stretch.Uniform;
+
+            image.Width = 500;
+            image.Height = 500;
+            image.Margin=new Thickness(10);
+            Grid grid = new Grid();
+            grid.Children.Add(image);
+
+        }
+    }
+
+}
